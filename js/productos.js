397a29ae125e4a64d03e4ea4552138f65c369b68
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-03-08 17:52:31 -0300

    "commit dom"

---
+++ js/productos.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Paleta",
+        precio: 20000,
+        img: "https://cdn.wallapop.com/images/10420/ek/3p/__/c10420p880291833/i3172133468.jpg?pictureSize=W320"
+    },
+    {
+        id: 1,
+        nombre: "Pelota",
+        precio: 700,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcV7Wav1J7N8iO8Fjj4PqG4O7Y1OVHgEIdBhAx5lwrQQ&s",
+    },
+    {
+        id: 3,
+        nombre: "Grip",
+        precio: 500,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnX1o808gk94pWDuCVtHGzGCoFfcIU7ZgdQ&usqp=CAU",
+    },
+    {
+        id: 4,
+        nombre: "Protector",
+        precio: 1000,
+        img: "https://padelife.com/wp-content/uploads/2018/04/protector-pala-de-padel.jpg",
+    },
+];
