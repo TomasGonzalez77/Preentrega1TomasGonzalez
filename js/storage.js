11e6b96a062da7074f0e34da153a45ce6a7ff6ad
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-03-11 16:48:52 -0300

    "commit local storage"

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,14 @@
 localStorage.getItem("saludo",);
-console.log ("Esta comprando" , saludo);
+localStorage.setItem('sudo', "")
+console.log("Esta comprando", saludo);
+
+// Opción 1 ->  localStorage.setItem(name, content)
+// Opción 2 ->localStorage.name = content
+// name = nombre del elemento
+// content = Contenido del elemento
+
+localStorage.setItem('Nombre', 'Miguel Antonio')
+localStorage.Apellido = 'Márquez Montoya'
 
 const verCarrito1JSON = JSON.stringify(verCarrito);
 console.log(verCarrito1JSON);
