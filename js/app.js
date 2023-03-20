1737b9ae124a5526cc265163c0c2235e7643ad79
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-03-20 17:14:20 -0300

    "commit"

--- js/app.js
+++ js/app.js
@@ -6,53 +6,46 @@ let saludo = prompt("Hola, ingresa tu nombre y apellido por favor");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const traerProductos = async () => {
-    const respuesta = await fetch("data.json");
-    const data = await respuesta.json();
-    data.forEach((product) => {
-        let content = document.createElement("div");
-        content.className = "card";
-        content.innerHTML = `
-        <img src="${product.img}"
-        <h3>${product.nombre}</h3>
-        <p class="precio">$ ${product.precio}</p>
-        `;
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}"
+    <h3>${product.nombre}</h3>
+    <p class="precio">$ ${product.precio}</p>
+    `;
 
-        contenidoTienda.append(content);
+    contenidoTienda.append(content);
 
-        let comprar = document.createElement("button")
-        comprar.innerText = "comprar";
-        comprar.className = "comprar";
+    let comprar = document.createElement("button")
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
 
-        content.append(comprar);
+    content.append(comprar);
 
-        comprar.addEventListener("click", () => {
+    comprar.addEventListener("click", () => {
 
-            const repetir = carrito.some((repetirProduct) => repetirProduct.id === product.id);
+        const repetir = carrito.some((repetirProduct) => repetirProduct.id === product.id);
 
-            if (repetir === true) {
-                carrito.map((prod) => {
-                    if (prod.id === product.id)
-                        prod.cantidad++;
-                });
-            } else {
+        if (repetir === true) {
+            carrito.map((prod) => {
+                if (prod.id === product.id)
+                    prod.cantidad++;
+            });
+        } else {
 
-                carrito.push({
-                    id: product.id,
-                    img: product.img,
-                    nombre: product.nombre,
-                    precio: product.precio,
-                    cantidad: product.cantidad,
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
 
-                });
-            }
-            console.log(carrito);
-            console.log(carrito.length);
-            carritoCounter();
-            guardadoLocal();
-        });
+            });
+        }
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        guardadoLocal();
     });
-
-}
-
-
+});
